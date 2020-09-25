a96d3fea50adee835bd0a1d31493923fcb5c92ad
jp <jptick@gmail.com>
2020-09-25 19:36:40 -0300

    Stylizing the Repasitory Detail

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,14 +1,66 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-interface RepositoryParams{
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
   repository: string;
 }
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars1.githubusercontent.com/u/66692428?s=460&u=f93cd9b6b7620c6176c53926b795bbda06ce001b&v=4"
+            alt="Avatar"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>descrição do repo</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>180</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>180</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>180</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="teste">
+          <div>
+            <strong>teste</strong>
+            <p>teste</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;

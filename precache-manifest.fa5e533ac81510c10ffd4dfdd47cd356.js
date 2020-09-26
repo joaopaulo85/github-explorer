5afe60fce086f2b4f840bf1967d63c819b541cd1
jp <jptick@gmail.com>
2020-09-26 10:05:12 -0300

    Updates

---
+++ precache-manifest.fa5e533ac81510c10ffd4dfdd47cd356.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32bb0613351f3c85d303674c4af76eb8",
+    "url": "/github-explorer/index.html"
+  },
+  {
+    "revision": "8a275df47b0124d6ff38",
+    "url": "/github-explorer/static/js/2.ffe223d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-explorer/static/js/2.ffe223d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63fce425544b52f6e5ec",
+    "url": "/github-explorer/static/js/main.e2eea940.chunk.js"
+  },
+  {
+    "revision": "b06b2c70688f513ae260",
+    "url": "/github-explorer/static/js/runtime-main.5251ac5e.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/github-explorer/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/github-explorer/static/media/logo.04f6798f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f47427b5bab20048ff706adabb1323b3.js"
+  "/github-explorer/precache-manifest.fa5e533ac81510c10ffd4dfdd47cd356.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-explorer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
